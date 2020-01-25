2d00950e6f2ee270280ed7cfb62b6066bdff5ab6
Manas <manas.rishav@gmail.com>
2020-01-25 23:36:08 +0530

    Update AddressInfo.java

--- src/main/java/com/manas/app/dao/AddressInfo.java
+++ src/main/java/com/manas/app/dao/AddressInfo.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+
+@NoArgsConstructor
 @Getter
 @Setter
 @AllArgsConstructor
